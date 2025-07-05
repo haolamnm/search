6cbadd07070dbe0b97af1ecb76b5ef6c8325009e
haolamnm <haolamnm@gmail.com>
2025-07-05 22:40:00 +0700

    fix yxyx boxes

--- services/common/types.py
+++ services/common/types.py
@@ -13,7 +13,7 @@ class ObjectRecord:
     _id: str
     detector: str
     scores: list[float] | None = None
-    yxyx_boxes: list[float] | None = None
+    yxyx_boxes: list[tuple[float, float, float, float]] | None = None
     labels: list[str] | None = None
     entities: list[str] | None = None
     names: list[str] | None = None
